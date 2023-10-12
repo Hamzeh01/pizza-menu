6609480887669ec0f880778ee6c5abbd56a73b6b
Husen Hamze <summer.00.projects@gmail.com>
2023-10-12 18:16:12 +0330

    add footer component

--- src/index.js
+++ src/index.js
@@ -113,4 +113,23 @@ function Pizza({ pizzaItem }) {
   );
 }
 
-function Footer() {}
+function Footer() {
+  const hour = new Date.getHours();
+  const openHour = 12;
+  const closeHour = 23;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  return (
+    <footer className="footer">
+      {isOpen ? (
+        <Order closeHour={closeHour} openHour={openHour} />
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
+      )}
+    </footer>
+  );
+}
+
+function Order({}) {}
